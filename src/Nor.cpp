b8ecf1bcc06240b970d1c70f7a16a378cff441a9
SergioMD15 <sergiomosquera96@gmail.com>
2019-05-07 17:03:30 +0200

    Final version

--- src/Nor.cpp
+++ src/Nor.cpp
@@ -30,36 +30,66 @@ public:
     initializeInputs();
     // First value has to be 1
     rel(*this, result[0] == 1);
+    // Last elements of the tree (leafs cannot be NOR gates)
     for(int i = 4 * max_nors; i < 4 * max_nodes; ++i) {
-      if((i - 1) % 4 == 0)
+      if((i - 1) % 4 == 0){
         rel(*this, result[i] > -1);
+      }
     }
     
-    // If a node is not a gate then the subreee rooted at it is all 0
-    for(int i = 1; i < 4*max_nodes; ++i) {
+    // // If a node is not a gate then the subreee rooted at it is all 0
+    for(int i = 1; i < 4 * max_nodes; ++i) {
       if((i - 1) % 4 == 0){
+        // The type of the node needs to be lower than the number of inputs
+        rel(*this, result[i] <= num_inputs);
+        // Index values must be positive
+        rel(*this, result[i-1] > 0);
+        // Inputs are followed by zeros
         rel(*this, (result[i] != -1) >> ((result[i+1] == 0) and (result[i+2] == 0)));
-        rel(*this, result[i+1] <= result[i+2]);
+        // NOR gates are followed by inputs greater than 1
+        rel(*this, (result[i] == -1) >> ((result[i+1] > 1) and (result[i+2] > 1)));
+        // NOR gates' left input is smaller than right input.
+        rel(*this, (result[i] == -1) >> ((result[i+1] < result[i+2])));
+
+        // Checking up to the last element
+        if(i != (4 * max_nodes - 3)){
+          // If we have a NOR gate, the left input would be defined immediately after the gate
+          rel(*this, (result[i] == -1) >> ((result[i+1] == result[i+3])));
+          // If we have a NOR gate, the left input has to be greater than the index of the NOR gate
+          rel(*this, (result[i] == -1) >> ((result[i+1] > result[i-1])));
+          // If we have two consecutive NOR gates, the second door inputs' have to be greater than
+          // the ones from the first.
+          rel(*this, (result[i] == -1 and result[i+4] == -1) >> ((result[i+2] < result[i+5])));
+          // The index of the door has to be different from the index of the next door.
+          rel(*this, result[i-1] != result[i+3]);
+        }
       }
     }
 
-    for (int i = 1; i < max_nodes; ++i){
 
-      // CONSTRAINTS FOR THE ELEMENTS
-      rel(*this, result[i] < max_nodes and result[i] >= -1);
-      if(i % 4 == 0){
-        rel(*this, result[i] != result[i-4]);
-        rel(*this, result[i] > 1);
-      }
-      if((i - 1) % 4 == 0){
-        // The elements index has to be positive
-        rel(*this, result[i] >= -1);
-        // The elements indicating the type of the node has to be
-        // either -1 or greater than 0
-        rel(*this, (result[i+1] != result[i+2]
-                and (result[i+2] > 1 and result[i+1] > 1 and result[i+1] < result[i+2]))
-                or (result[i+1] == 0 and result[i+2] == 0));
-      }
+
+
+
+
+
+
+    // for (int i = 1; i < max_nodes; ++i){
+
+    //   // CONSTRAINTS FOR THE ELEMENTS
+    //   rel(*this, result[i] < max_nodes and result[i] >= -1);
+    //   if(i % 4 == 0){
+    //     rel(*this, result[i] != result[i-4]);
+    //     rel(*this, result[i] > 1);
+    //   }
+    //   if((i - 1) % 4 == 0){
+    //     // The elements index has to be positive
+    //     rel(*this, result[i] >= -1);
+    //     // The elements indicating the type of the node has to be
+    //     // either -1 or greater than 0
+    //     rel(*this, (result[i+1] != result[i+2]
+    //             and (result[i+2] > 1 and result[i+1] > 1 and result[i+1] < result[i+2]))
+    //             or (result[i+1] == 0 and result[i+2] == 0));
+    //   }
         // CONSTRAINTS FOR THE INPUTS
         // if(result[i].val() != -1){
           // // Inputs or zero values cannot have inputs
@@ -79,20 +109,20 @@ public:
         //   // immediately after the NOR gate.
         //   rel(*this, result[i+1] == result[i+3]);
         // }
-    }
+    // }
 
     // At most 2^(depth) - 1 NOR gates in the circuit.
-    count(*this, result, -1, IRT_LQ, max_nors);
+    // count(*this, result, -1, IRT_LQ, max_nors);
 
-    // If the array size is greater than 1, every value greater than 1 needs
-    // to appear twice in the solution array (once when it is used as input
-    // and another one when it is defined).
-    if(result.size() > 1){
-      for(int i = 2; i < max_nodes + 1; ++i){
-        if(i%4 == 0)
-          count(*this, result, i, IRT_EQ, 1);
-      }
-    }
+    // // If the array size is greater than 1, every value greater than 1 needs
+    // // to appear twice in the solution array (once when it is used as input
+    // // and another one when it is defined).
+    // if(result.size() > 1){
+    //   for(int i = 2; i < max_nodes + 1; ++i){
+    //     if(i%4 == 0)
+    //       count(*this, result, i, IRT_EQ, 1);
+    //   }
+    // }
 
     branch(*this, result, INT_VAR_NONE(), INT_VAL_MIN());
   }
@@ -146,12 +176,18 @@ public:
     return counter;
   }
 
+  virtual void constrain(const Space& _b) {
+    const Nor& nor = static_cast <const Nor&>( _b );
+  }
+
   Nor(Nor &s) : Space(s)
   {
     num_inputs = s.num_inputs;
     depth = s.depth;
     max_nodes = s.max_nodes;
+    max_nors = s.max_nors;
     inputs = s.inputs;
+
     result.update(*this, s.result);
   }
 
@@ -196,57 +232,73 @@ public:
   */
   VI getInputs(Nor* solution, int index){
     int num_inputs = solution -> getNum_inputs();
+    VI copy(pow(2, num_inputs));
+    if(index == -1){
+      copy[0] = -1;
+      return copy;
+    }
     int start = index * pow(2, num_inputs);
     int end = (index + 1) * pow(2, num_inputs);
 
     VB inputs = solution -> getInputs();
-    VI copy(pow(2, num_inputs));
     for(int i = start; i < end; i++){
       copy[i] = inputs[i];
     }
     return copy;
   }
 
-  int findIndex(IntVarArray list, int index){
+  int findIndex(IntVarArray list, int value){
     // We have the value for the result
     // and we want to find the index
-    if(index == 0){
+    if(value == 0){
       return 0;
     }
     else{
       for(int i = 1; i < list.size(); i++){
-        if((i - 1) % 4 == 0 && list[i].val() == index){
+        if((i - 1) % 4 == 0 && list[i].val() == value){
           return i;
         }
       }
     }
+    return -1;
+  }
+
+  bool anyIndexIsZero(IntVarArray list, int index){
+    if(list[index + 1].val() != 0)
+      return ((list[index + 2].val() == 0) and (list[index + 3].val() == 0));
   }
 
   VI norOperation(Nor* solution, int index){
     IntVarArray list = solution -> getResult();
-    if(solution->getDepth() == 0){
+    if(solution->getDepth() == 0 or anyIndexIsZero(list, index)){
       return getInputs(solution, list[1].val());
     }
 
     // We find the position of the NOR gate inputs'
     // identifiers (1 -1 2 3  --> Looking where are 2
     // and 3 in the array).
-    int index_first = findIndex(list, index + 2);
-    int index_second = findIndex(list, index + 3);
+    int index_first = findIndex(list, list[index + 2].val());
+    int index_second = findIndex(list, list[index + 3].val());
     VI left, right;
+    if(index_first == -1 or index_second == -1)
+      return left;
     
     // Get the inputs for the left part. If it
     // is a NOR gate, we iterate again until
     // getting a valid input.
     if(list[index_first + 1].val() != -1){
-      left = getInputs(solution, index_first + 1);
+      left = getInputs(solution, list[index_first + 1].val());
+      if(left[0] == -1)
+        return left;
     } else{
       left = norOperation(solution, index_first);
     }
 
     // Same as before with the right side.
     if(list[index_second + 1].val() != -1){
-      right = getInputs(solution, index_first + 1);
+      right = getInputs(solution, list[index_second + 1].val());
+      if(right[0] == -1)
+        return right;
     } else{
       right = norOperation(solution, index_first);
     }
@@ -254,18 +306,12 @@ public:
   }
 
 Nor* compare(Nor* first, Nor* second){
-  if(first -> getDepth() > second -> getDepth()){
+  if(first->count_gates() > second->count_gates()){
     return second;
-  } else if (first->getDepth() < second->getDepth()){
+  } else if (first->count_gates() < second->count_gates()){
     return first;
   } else{
-    if(first->count_gates() > second->count_gates()){
-      return second;
-    } else if (first->count_gates() < second->count_gates()){
-      return first;
-    } else{
-      return first;
-    }
+    return first;
   }
 }
 
@@ -277,7 +323,7 @@ int main(int argc, char *argv[])
     int n, u;
     cin >> n;
     VI g(pow(2, n));
-    const int max_depth = 4;
+    const int max_depth = 3;
     for (int k = 0; k < pow(2, n); ++k)
     {
       cin >> u;
@@ -286,34 +332,27 @@ int main(int argc, char *argv[])
 
     for(int j = 0; j < max_depth; j++){
       if(valid_solution){
+        valid_solution->print(g);
         break;
       }
       cout << endl << "Looking for a solution with depth " << j << endl;
       Nor* mod = new Nor(g, n, j);
-      BAB<Nor> e(mod);
+      DFS<Nor> e(mod);
       delete mod;
-      Nor* s = e.next();
-      while(s != NULL){
-        VI result =  norOperation(s, 0);
+      while(Nor* s = e.next()){
         bool valid = true;
-        for(int i = 0; i < result.size(); i++){
-          if(result[i] != g[i]){
-            valid = false;
-            break;
-          }
+        s->print(g);
+        VI result = norOperation(s, 0);
+        if(result != g){
+          valid = false;
         }
         if(valid){
           cout << "SOLUTION FOUND" << endl;
-          if(valid_solution)
-            valid_solution = compare(s, valid_solution);
-          else{
-            valid_solution = s;
-          }
+          valid_solution = s;
           break;
         }
       }
     }
-    valid_solution->print(g);
   }
   catch (Exception e)
   {
